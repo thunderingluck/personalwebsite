71e92cea84962aff6eb432857fa68f25062b1c31
Eva Ge <evag3@Sapphire.mit.edu>
2025-11-30 23:09:50 -0500

    Added name

--- app/header.tsx
+++ app/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className="mb-8 flex items-center justify-between">
       <div>
         <Link href="/" className="font-medium text-black dark:text-white">
-          Julien Nim
+          Eva Ge
         </Link>
         <TextEffect
           as="p"
